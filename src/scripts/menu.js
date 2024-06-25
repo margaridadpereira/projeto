b82274e45fd09f76bce9a39896109d03ce0c7cde
margaridadpereira <margaridap.design@gmail.com>
2024-06-25 19:57:46 +0100

    last updates

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,12 +1,3 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
   });
-  
-export default function MarkdownPostLayout({ children, ...props }) {
-  console.log('MarkdownPostLayout props:', props);
-  return (
-    <div>
-      {children}
-    </div>
-  );
-}
